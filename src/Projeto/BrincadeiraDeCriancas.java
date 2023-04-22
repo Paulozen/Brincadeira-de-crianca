53fb6c7662a5f0ffc7c41f3a2691d34687c1dd5f
Paulozen <60679251+Paulozen@users.noreply.github.com>
2023-04-22 18:07:53 -0300

    Concluindo modificações Finais
    Conclui as modificações finais, novas implementações, ficou pendente limpeza de código

--- src/Projeto/BrincadeiraDeCriancas.java
+++ src/Projeto/BrincadeiraDeCriancas.java
@@ -6,24 +6,32 @@ import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.concurrent.Semaphore;
 import Projeto.Janela;
+import javax.swing.*;
 
 
 public class BrincadeiraDeCriancas {
     private static final int NUM_CRIANCAS = 10;
-    private static final int CAPACIDADE_CESTO = 10;
-    private static Semaphore semaforoCestoCheio = new Semaphore(CAPACIDADE_CESTO); // capacidade do cesto
+    private static Semaphore semaforoCestoCheio = new Semaphore(0); // capacidade do cesto
     private static Semaphore semaforoCestoVazio = new Semaphore(0); // quantidade de bolas inicial
+    //static ImageIcon imagemCriancaVazio = new ImageIcon(getClass().getResource("/resources/vazio.png"));
+    //static ImageIcon imagemCrianca2 = new ImageIcon(getClass().getResource("/resources/fundo.png"));
+
     
     
     public static void adicionarCrianca(String id, boolean comecaComBola, Integer tempoBrincando, Integer tempoOutraAtividad) {
     	
     	  int numeroInt = Integer.parseInt(id);
-    	
+          //System.out.println("criança 1:" + id + comecaComBola + tempoBrincando + tempoOutraAtividad);
     	  Crianca crianca = new Crianca(numeroInt,comecaComBola,tempoBrincando,tempoOutraAtividad);
           crianca.start();
     	
     	
        
+    }
+
+    static void definirSemaforoCesto(int i){
+       semaforoCestoCheio.release(i);
+       Janela.LabelCapacidadeCesto.setIcon(Janela.imagemCapacidadeCesto[i-1]);
     }
 //
 //    public static void main(String[] args) {
@@ -33,18 +41,30 @@ public class BrincadeiraDeCriancas {
 //        }
 //    }
     
-    public static void Espera(int tempo) {
-    	LocalDateTime inicio = LocalDateTime.now();    	
-    	
+    public static void Espera(int tempo,int opcao,int id) {
+    	LocalDateTime inicio = LocalDateTime.now(); 
+        
     	while(true) {
     		LocalDateTime agora = LocalDateTime.now();
-    		
+            if( opcao == 1){
+    		if(ChronoUnit.SECONDS.between(inicio, agora)%2 == 1){
+                Janela.LabelCrianca[id-1].setIcon(Janela.imagemCriancaBrincando1);
+            }else{
+                Janela.LabelCrianca[id-1].setIcon(Janela.imagemCriancaBrincando2);
+            }}
     		if(ChronoUnit.SECONDS.between(inicio, agora) >= tempo) {
     			return;
     			}
     	}
     	
     }
+    public static void AdicionarBola() {
+        semaforoCestoVazio.release();
+        Janela.LabelQuantidadeAtual.setIcon(Janela.imagemQuantidadeAtual[semaforoCestoVazio.availablePermits()]);
+    	
+    }
+
+    
     
     
 
@@ -67,22 +87,28 @@ public class BrincadeiraDeCriancas {
                     if (comBola) {
                         Janela.campoLogs.append("Crian�a " + id + " começou com bola e está brincando"  + "\n");
                         System.out.println("Crian�a " + id + " começou com bola e está brincando");
-                        comBola = true;            
+                        comBola = true;
+                        Janela.LabelCrianca[id-1].setIcon(Janela.imagemCriancaBrincando1);            
                         
                     }else{
                         Janela.campoLogs.append("Crian�a " + id + " tentou pegar uma bola"  + "\n");
                         System.out.println("Crian�a " + id + " tentou pegar uma bola");
+                        Janela.LabelCrianca[id-1].setIcon(Janela.imagemCriancaTentandoPegar);   
                         semaforoCestoVazio.acquire();
                         semaforoCestoCheio.release();
+                        Janela.LabelQuantidadeAtual.setIcon(Janela.imagemQuantidadeAtual[semaforoCestoVazio.availablePermits()]);
                         comBola = true; 
                         Janela.campoLogs.append("Crian�a " + id + " pegou uma bola do cesto e est� brincando"  + "\n");
-                        System.out.println("Crian�a " + id + " pegou uma bola do cesto e est� brincando");                                     
+                        System.out.println("Crian�a " + id + " pegou uma bola do cesto e est� brincando");  
+                        Janela.LabelCrianca[id-1].setIcon(Janela.imagemCriancaBrincando1);                                    
                     }                    
-                    Espera(tempoBrincando);
+                    Espera(tempoBrincando,1,id);
                     System.out.println("Crian�a " + id + " tentou colocar a bola no cesto");
                     Janela.campoLogs.append("Crian�a " + id + " tentou colocar a bola no cesto"+ "\n");
+                    Janela.LabelCrianca[id-1].setIcon(Janela.imagemCriancaTentandoColocar);   
                     semaforoCestoCheio.acquire();
                     semaforoCestoVazio.release();
+                    Janela.LabelQuantidadeAtual.setIcon(Janela.imagemQuantidadeAtual[semaforoCestoVazio.availablePermits()]);
                     System.out.println("Crian�a " + id + " colocou a bola no cesto");
                     Janela.campoLogs.append("Crian�a " + id + " colocou a bola no cesto"+ "\n");
                     comBola = false;
@@ -90,7 +116,8 @@ public class BrincadeiraDeCriancas {
 
                     System.out.println("Criança " + id + " esta em outra atividade");
                     Janela.campoLogs.append("Criança " + id + " esta em outra atividade"+ "\n");
-                    Espera(tempoOutraAtividade);
+                    Janela.LabelCrianca[id-1].setIcon(Janela.imagemCriancaOutraAtv1); 
+                    Espera(tempoOutraAtividade,0,id);
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
--- src/Projeto/Janela.java
+++ src/Projeto/Janela.java
@@ -20,13 +20,27 @@ import java.util.concurrent.Semaphore;
 public class Janela extends JFrame {
 
     private ImageIcon imagemCrianca1;
-    private ImageIcon imagemCrianca2;
+    static public ImageIcon imagemCriancaVazio;
+    static public ImageIcon imagemCrianca2;
+    static public ImageIcon imagemCriancaBrincando1;
+    static public ImageIcon imagemCriancaBrincando2;
+    static public ImageIcon imagemCriancaTentandoPegar;
+    static public ImageIcon imagemCriancaTentandoColocar;
+    static public ImageIcon imagemCriancaOutraAtv1;
+    static public ImageIcon imagemCesto;
+    static public ImageIcon[] imagemCapacidadeCesto = new ImageIcon[10];
+    static public ImageIcon[] imagemQuantidadeAtual = new ImageIcon[11];
     private JLabel labelCrianca1;
     private JLabel labelCrianca2;
     private boolean movimentaCrianca1;
     private boolean movimentaCrianca2;   
     private Timer timer;
     private int x, y;
+
+    public static JLabel[] LabelCrianca = new JLabel[10];
+    public static JLabel LabelCapacidadeCesto = new JLabel();
+    public static JLabel LabelQuantidadeAtual = new JLabel();
+
     JPanel painelCrianca = new JPanel();
     
     
@@ -48,12 +62,16 @@ public class Janela extends JFrame {
     
     public Janela() {
 
-       
+        
+
+
+
+
         // Define o t�tulo da janela
         setTitle("Brincadeira de criança");
 
         // Define o tamanho da janela
-        setSize(1920, 1080);
+        setSize(1080, 720);
 
         // Define a opera��o padr�o ao fechar a janela
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -140,68 +158,163 @@ public class Janela extends JFrame {
 
         // Cria um campo de texto para exibir os logs
         
-        scrollPane.setPreferredSize(new Dimension(300, 300));
+        scrollPane.setPreferredSize(new Dimension(300, 100));
         
          // Cria um painel para a imagem de fundo e as imagens das crian�as
-    JPanel painelImagem = new JPanel() {
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            ImageIcon fundo = new ImageIcon(getClass().getResource("/resources/fundo.png"));
-            g.drawImage(fundo.getImage(), 0, 0, getWidth(), getHeight(), this);
+    // JPanel painelImagem = new JPanel() {
+    //     @Override
+    //     protected void paintComponent(Graphics g) {
+    //         super.paintComponent(g);
+    //         ImageIcon fundo = new ImageIcon(getClass().getResource("/resources/fundo.png"));
+    //         g.drawImage(fundo.getImage(), 0, 0, getWidth(), getHeight(), this);
            
-        }
-    };
+    //     }
+    // };
+
+    JPanel painelImagem = new JPanel(new GridLayout(2, 6));
                 
         
 
         // Adiciona os pain�is ao painel principal
         JPanel painelPrincipal = new JPanel(new BorderLayout());
-        painelPrincipal.add(painelConfiguracaoInicial, BorderLayout.NORTH);
+        //painelPrincipal.add(painelConfiguracaoInicial, BorderLayout.NORTH);
         painelPrincipal.add(painelImagem, BorderLayout.CENTER);
         painelPrincipal.add(painelConfiguracao, BorderLayout.EAST);
+        imagemCriancaVazio = new ImageIcon(getClass().getResource("/resources/vazio.png"));
+        //JLabel LabelCrianca1 = new JLabel(imagemCriancaVazio);
+        imagemCrianca2 = new ImageIcon(getClass().getResource("/resources/fundo.png"));
+        imagemCriancaBrincando1 = new ImageIcon(getClass().getResource("/resources/crianca_brincando_1.png"));
+        imagemCriancaBrincando2 = new ImageIcon(getClass().getResource("/resources/crianca_brincando_2.png"));
+        imagemCriancaOutraAtv1 = new ImageIcon(getClass().getResource("/resources/crianca_outraatv_1.png"));
+        imagemCesto = new ImageIcon(getClass().getResource("/resources/cesto.png"));
+        imagemCriancaTentandoPegar = new ImageIcon(getClass().getResource("/resources/crianca_tentando_pegar.png"));
+        imagemCriancaTentandoColocar = new ImageIcon(getClass().getResource("/resources/crianca_tentando_colocar.png"));
+        imagemQuantidadeAtual[10] = new ImageIcon(getClass().getResource("/resources/quantidadecesto/10.png"));
+        for(int i = 0; i<10; i++){
+            int j= i+1;
+            imagemCapacidadeCesto[i] = new ImageIcon(getClass().getResource("/resources/capacidadecesto/"+j+".png"));
+            imagemQuantidadeAtual[i] = new ImageIcon(getClass().getResource("/resources/quantidadecesto/"+i+".png"));
+            LabelCrianca[i] = new JLabel(imagemCriancaVazio);                       
+        }
+        LabelCapacidadeCesto = new JLabel(imagemCapacidadeCesto[0]); 
+        LabelQuantidadeAtual = new JLabel(imagemQuantidadeAtual[0]); 
+
+
+        
+        //painelCrianca = new JPanel(new BorderLayout());
+        JPanel[] painelCrianca = new JPanel[10];
+        for(int i = 0; i<10; i++){
+            painelCrianca[i] = new JPanel(new BorderLayout());
+            painelCrianca[i].setOpaque(false); // torna o painel transparente
+            painelCrianca[i].add(LabelCrianca[i]);
+            painelImagem.add(painelCrianca[i]);
+        }
+        painelImagem.add(LabelCapacidadeCesto);
+        painelImagem.add(LabelQuantidadeAtual);
+       
+        
+        
+        
+        
+
         JButton botaoAdicionar = new JButton("Adicionar crian�a");
         painelConfiguracao.add(botaoAdicionar);
-        
+       
                 // Define o comportamento do bot�o de adicionar crian�a
                 botaoAdicionar.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         AdicionarPainelCrianca();
+                        //LabelCrianca[0].setIcon(imagemCrianca2);
                         String nome = campoNome.getText();
                         Integer intTempoBrincando = Integer.parseInt(TempoBrincando.getText());
                         Integer intTempoOutraAtividade = Integer.parseInt(TempoOutraAtividade.getText());
                         boolean comecaComBola = checkBoxBola.isSelected();
                        // Janela.campoLogs.append("TESTE SE ESTA COM BOLA" + comecaComBola);
                         BrincadeiraDeCriancas.adicionarCrianca(nome, comecaComBola,intTempoBrincando,intTempoOutraAtividade);
+                        
+                    }
+                });
+
+       
+        
+    
+
+
+        JButton botaoAdicionarBola = new JButton("Adicionar Bola");
+        painelConfiguracao.add(botaoAdicionarBola);
+       
+                // Define o comportamento do bot�o de adicionar crian�a
+                botaoAdicionarBola.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        BrincadeiraDeCriancas.AdicionarBola();
+                        
                     }
                 });
         painelPrincipal.add(scrollPane, BorderLayout.SOUTH);
 
-        imagemCrianca1 = new ImageIcon(getClass().getResource("/resources/crianca.jpg"));
-        JLabel labelCima = new JLabel(imagemCrianca1);
-        painelCrianca = new JPanel(new BorderLayout());
-        painelCrianca.setOpaque(false); // torna o painel transparente
-        painelCrianca.add(labelCima);
-        painelImagem.add(painelCrianca, BorderLayout.CENTER);
+       
+        
         
 
-        timer = new Timer(10, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Atualiza as coordenadas da imagem de cima
-                x += 4; // atualiza a coordenada x (pode ser alterado para o valor desejado)
+        // timer = new Timer(10, new ActionListener() {
+        //     @Override
+        //     public void actionPerformed(ActionEvent e) {
+        //         // Atualiza as coordenadas da imagem de cima
+        //         x += 4; // atualiza a coordenada x (pode ser alterado para o valor desejado)
                 
-                painelCrianca.setLocation(x, y); // define a nova posição da imagem de cima
-            }
-        });
-        timer.start(); // inicia o timer para começar a movimentação
+        //         painelCrianca.setLocation(x, y); // define a nova posição da imagem de cima
+        //     }
+        // });
+        //timer.start(); // inicia o timer para começar a movimentação
        
         // Adiciona o painel principal � janela
         getContentPane().add(painelPrincipal);
 
         // Exibe a janela
         setVisible(true);
+
+        int CapacidadeCesto = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a capacidade do cesto (Ex. 10)", "Qual a capacidade do Cesto?",  JOptionPane.QUESTION_MESSAGE));
+        BrincadeiraDeCriancas.definirSemaforoCesto(CapacidadeCesto);
+        
+        
+        
+        
+        int QuantidadeInicial = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a quantidade inicial de Crianças (Ex. 3)", "Qual a quantidade inicial de Crianças?",  JOptionPane.QUESTION_MESSAGE));
+        if(QuantidadeInicial != 0){
+            String QuaisComBola = JOptionPane.showInputDialog(null, "Quais crianças começam com bola?","Digite quais crianças começam com bola, separadas por virgula: (Ex. 1,3)",  JOptionPane.QUESTION_MESSAGE);
+        if(QuaisComBola != "0"){
+        int[] intComecaComBola = separarVirgula(QuaisComBola);        
+        String TempoCriancaBrincando = JOptionPane.showInputDialog(null, "Digite qual tempo de cada criança brincando, separadas por virgula: (Ex. 5,7,4)", "Qual tempo de cada criança brincando?",  JOptionPane.QUESTION_MESSAGE);
+        String TempoCriancaOutraAtv = JOptionPane.showInputDialog(null,"Digite qual tempo de cada criança brincando, separadas por virgula: (Ex. 9,5,3)", "Qual tempo de cada criança em outra atividade?",  JOptionPane.QUESTION_MESSAGE);
+        
+
+        
+            
+        int[] intTempoBrincando = separarVirgula(TempoCriancaBrincando);        
+        int[] intTempoOutraAtividade = separarVirgula(TempoCriancaOutraAtv);
+
+        // System.out.println(intComecaComBola[0]);
+        // System.out.println(intComecaComBola[1]);
+        // System.out.println(intComecaComBola[2]);
+
+        for(Integer i = 0; i < QuantidadeInicial; i++){
+            boolean boolComecaComBola = false;
+            String id = Integer.toString(i+1);
+            for(Integer j = 0; j<intComecaComBola.length;j++){
+                if(intComecaComBola[j] == i+1){
+                    boolComecaComBola = true;
+                    break;
+                }
+            }
+            //System.out.println("Janela:" + id + boolComecaComBola + intTempoBrincando[i] + intTempoOutraAtividade[i]);
+            BrincadeiraDeCriancas.adicionarCrianca(id,boolComecaComBola,intTempoBrincando[i],intTempoOutraAtividade[i]);
+
+
+        }
+        }
+    }
     }
 
     
